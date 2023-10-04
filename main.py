8cc129f0bc303e8bfe8f17dce1fb0dd7a391edb4
lemmenn <nina.lemmens@hva.nl>
2023-10-04 18:17:51 +0200

    Task 2 - extract relevant features

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn import svm
 
 def main():
     training_r, training_s, test_r, test_s = split_data()
+    training_review_words = extract_features(training_r, 4, 100)
 
 
 # TASK 1
@@ -27,6 +28,35 @@ def split_data():
     print("Negative reviews in test set: ", test_sentiments.value_counts()["negative"])
 
     return training_reviews, training_sentiments, test_reviews, test_sentiments
+
+
+# TASK 2
+def extract_features(training_r, min_word_length, min_word_occurrence):
+    word_occurrences = {}
+    filtered_words = []
+
+    # Remove non-alphanumeric values from reviews
+    reviews = training_r.str.replace('[^a-zA-Z0-9\s]', '', regex=True)
+
+    # Convert reviews to lowercase
+    reviews = reviews.str.lower()
+
+    # Split reviews into individual words
+    words = reviews.to_string(index=False).split()
+
+    # Extract words that meet requirements
+    for word in words:
+        if len(word) >= min_word_length:
+            if word in word_occurrences:
+                word_occurrences[word] = word_occurrences[word] + 1
+            else:
+                word_occurrences[word] = 1
+
+    for word in word_occurrences:
+        if word_occurrences[word] >= min_word_occurrence:
+            filtered_words.append(word)
+
+    return filtered_words
 
 
 main()
